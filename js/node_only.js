8bda41986e3cc094c44d488c67fd26c6200034a9
Brian Walk <MacDaddy2@Brians-iMac.local>
2015-10-05 20:34:05 -0500

    added css and js files to site.

---
+++ js/node_only.js
@@ -0,0 +1,19 @@
+/* ==========================================================================
+   Node only scripts - Only to be used via node command line tool
+   				     - this file is NOT included in index.html
+   ========================================================================== */
+   
+// var name = "Brian Walk",
+// var character = "Warlock Jones";
+
+// console.log('Hi, my name is ' + name + ' and my character name is ' + character + '.');
+
+ var name = "Student";
+ var age = 21;
+ var profession = "Graphic Designer";
+
+ // var nombre = prompt('What is your new name?'),
+ // 	 edad = 21,
+ // 	 profesion = "disenador grafico";
+
+console.log('Hi my name is ' + name + ' and I am ' + age + ' and by trade I am a ' + profession);
